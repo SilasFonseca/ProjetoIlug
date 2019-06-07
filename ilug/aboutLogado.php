d79081a512f3bcdb66185a683abfea99dd2f5fc1
silas <silas.fonseca1@hotmail.com>
2019-06-07 17:38:01 -0300

    Create aboutLogado.php

---
+++ ilug/aboutLogado.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title>Sobre ILUG &mdash; Site de aluguel de imóveis</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	  <link rel="stylesheet" href="fonts/icomoon/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="css/aos.css">
+    <link rel="stylesheet" href="css/rangeslider.css">
+    <link rel="stylesheet" href="css/style.css">    
+  </head>
+  <body>
+  <div class="site-wrap">
+
+    <div class="site-mobile-menu">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div>
+    
+<?php 
+  session_start();
+include 'headerLogado.php'; ?>
+  
+    
+   <div class="site-blocks-cover inner-page-cover overlay" style="background-image: url(images/background_casas.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+      <div class="container">
+        <div class="row align-items-center justify-content-center text-center">
+
+          <div class="col-md-10" data-aos="fade-up" data-aos-delay="400">
+            
+            
+            <div class="row justify-content-center mt-5">
+              <div class="col-md-8 text-center">
+                <h1>Sobre o Ilug</h1>
+                <p class="mb-0"></p>
+              </div>
+            </div>
+
+            
+          </div>
+        </div>
+      </div>
+    </div>  
+
+    <div class="site-section"  data-aos="fade">
+      <div class="container">
+        
+       
+        <div class="row mb-12">
+          <div class="col-md-12 ml-auto">
+            <p>
+	        O Projeto do desenvolvimento do aplicativo iLug surgiu a partir da dificuldade de pessoas em
+			localizar, de forma fácil e segura, imóveis para alugar. O aplicativo iLug irá listar locais exatos de
+			imóveis para alugar em Maceió e, além disso, permitirá ao usuário pesquisar por bairros e filtrar
+			resultados por faixa de preço. Terá, também, total integração com o Google Maps para auxiliar o
+
+			usuário a procurar um imóvel no modo Pesquisar no Mapa. Fornecerá informações como nome para
+			contato, contatos, endereço e preço. O aplicativo, a princípio, não terá o poder de alugar a casa mas
+			fornecerá ao usuário o máximo de informações possíveis para facilitar a escolha do local ideal do seu
+			imóvel.
+			</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+<?php include 'footer.php'; ?>
+  </div>
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/bootstrap-datepicker.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/rangeslider.min.js"></script>
+
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
