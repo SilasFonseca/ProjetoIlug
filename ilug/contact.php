5808229385e3779839fe107c4d0bced3b96cc19e
silas <silas.fonseca1@hotmail.com>
2019-06-07 17:39:37 -0300

    Create contact.php

---
+++ ilug/contact.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title>Contato ILUG &mdash; Site de aluguel de imóveis</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="css/aos.css">
+    <link rel="stylesheet" href="css/rangeslider.css">
+    <link rel="stylesheet" href="css/style.css">    
+  </head>
+  <body>
+  
+  <div class="site-wrap">
+
+    <div class="site-mobile-menu">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div>
+    
+  <?php include 'header.php'; ?>
+
+  
+    
+    <div class="site-blocks-cover inner-page-cover overlay" style="background-image: url(images/background_casas.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+      <div class="container">
+        <div class="row align-items-center justify-content-center text-center">
+
+          <div class="col-md-10" data-aos="fade-up" data-aos-delay="400">
+            
+            
+            <div class="row justify-content-center mt-5">
+              <div class="col-md-8 text-center">
+                <h1>Contate-nos</h1>
+                <p class="mb-0">Gostaria de falar conosco?</p>
+              </div>
+            </div>
+
+            
+          </div>
+        </div>
+      </div>
+    </div>  
+
+    <div class="site-section bg-light">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-7 mb-5"  data-aos="fade">
+
+            
+
+            <form action="#" class="p-5 bg-white">
+             
+
+              <div class="row form-group">
+                <div class="col-md-6 mb-3 mb-md-0">
+                  <label class="text-black" for="fname">Nome</label>
+                  <input type="text" id="fname" class="form-control">
+                </div>
+                <div class="col-md-6">
+                  <label class="text-black" for="lname">Sobrenome</label>
+                  <input type="text" id="lname" class="form-control">
+                </div>
+              </div>
+
+              <div class="row form-group">
+                
+                <div class="col-md-12">
+                  <label class="text-black" for="email">Email</label> 
+                  <input type="email" id="email" class="form-control">
+                </div>
+              </div>
+
+              <div class="row form-group">
+                <div class="col-md-12">
+                  <label class="text-black" for="message">Mensagem</label> 
+                  <textarea name="message" id="message" cols="30" rows="7" class="form-control" placeholder="Digite aqui sua mensagem"></textarea>
+                </div>
+              </div>
+
+              <div class="row form-group">
+                <div class="col-md-12">
+                  <input type="submit" value="Enviar" class="btn btn-primary py-2 px-4 text-white">
+                </div>
+              </div>
+
+  
+            </form>
+          </div>
+          <div class="col-md-5"  data-aos="fade" data-aos-delay="100">
+            
+            <div class="p-4 mb-3 bg-white">
+              <p class="mb-0 font-weight-bold">Endereço</p>
+              <p class="mb-4">Cep:<span class="text-primary"> 57073-470</span><br/>
+                Rua Menino Marcelo, 3.800 – Shopping Pátio Maceió</p>
+
+              <p class="mb-0 font-weight-bold">Telefone</p>
+              <p class="mb-4"><a href="#">(82)4002-8922</a></p>
+
+              <p class="mb-0 font-weight-bold">Email</p>
+              <p class="mb-0"><a href="#">suporte@ilug.com</a></p>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section">
+      <div class="container">
+        <div class="row justify-content-center mb-5">
+          <div class="col-md-7 text-center border-primary">
+            <h2 class="font-weight-light text-primary">Perguntas Frequentes</h2>
+            <p class="color-black-opacity-5">Caso sua duvida for as abaixo</p>
+          </div>
+        </div>
+
+
+        <div class="row justify-content-center">
+          <div class="col-8">
+            <div class="border p-3 rounded mb-2">
+              <a data-toggle="collapse" href="#collapse-1" role="button" aria-expanded="false" aria-controls="collapse-1" class="accordion-item h5 d-block mb-0">Como Anunciar?</a>
+
+              <div class="collapse" id="collapse-1">
+                <div class="pt-2">
+                  <p class="mb-0">Para anunciar basta clicar no botão no canto superior da tela e preencher os campos solicitados</p>
+                </div>
+              </div>
+            </div>
+
+            <div class="border p-3 rounded mb-2">
+              <a data-toggle="collapse" href="#collapse-4" role="button" aria-expanded="false" aria-controls="collapse-4" class="accordion-item h5 d-block mb-0">Como se cadastrar?</a>
+
+              <div class="collapse" id="collapse-4">
+                <div class="pt-2">
+                  <p class="mb-0">para se cadastrar, basta clicar no botão cadastrar-se que se encontra no menú em cima e preencher todos os dados solicitados</p>
+                </div>
+              </div>
+            </div>
+
+            <div class="border p-3 rounded mb-2">
+              <a data-toggle="collapse" href="#collapse-2" role="button" aria-expanded="false" aria-controls="collapse-2" class="accordion-item h5 d-block mb-0">Gostaria de saber sobre a empresa?</a>
+
+              <div class="collapse" id="collapse-2">
+                <div class="pt-2">
+                  <p class="mb-0">Clique no botão Sobre que se encontra no menú principal</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+        
+      </div>
+    </div> 
+    
+<?php include 'footer.php'; ?>
+  </div>
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/bootstrap-datepicker.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/rangeslider.min.js"></script>
+
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
